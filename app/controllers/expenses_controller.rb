40f846fc6c17c700930193842c873a2a4cce9522
weijuemin <weijueminjas@gmail.com>
2016-08-23 11:55:04 -0700

    Added algorithm of debt relationship, ouputs in controller:expenses, show.html.erb - later we should put it in Ajax

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -5,6 +5,44 @@ class ExpensesController < ApplicationController
 	def create
 		redirect_to "/group/#{ params[:group_id] }"
 	end
+
 	def show
+    @exp = Expense.find(params[:expense_id])
+    allrecord = Record.where(expense: @exp).all
+    owes, owed = [], []
+
+    allrecord.each do |r|
+      diff = r.paid - r.amount
+      if diff < 0
+        owes << {:user => r.user, :diff => diff}
+      elsif diff > 0
+        owed << {:user => r.user, :diff => diff}
+      end
+    end
+
+    @output =[]
+    p, n = owed, owes
+
+    puts p
+    puts n
+    while 0<p.length || 0<n.length
+      if p[0][:diff] < n[0][:diff].abs
+        amt = p[0][:diff]
+        @output << {:owes =>n[0][:user], :owed =>p[0][:user], :amt => amt}
+        n[0][:diff] += p[0][:diff]
+        p.shift
+      elsif p[0][:diff] > n[0][:diff].abs
+        amt = n[0][:diff].abs
+        @output << {:owes =>n[0][:user], :owed =>p[0][:user], :amt => amt}
+        p[0][:diff] += n[0][:diff]
+        n.shift
+      elsif p[0][:diff] = n[0][:diff].abs
+        amt = p[0][:diff]
+        @output << {:owes =>n[0][:user], :owed =>p[0][:user], :amt => amt}
+        p.shift
+        n.shift
+      end
+    end
+    @output
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,9 @@ Rails.application.routes.draw do
 
   post '/users/create' => 'users#create'
 
-	get 'group/:group_id' => 'groups#show'
-	get 'expense/:expense_id' => 'expenses#show'
-	get 'expense/:group_id/new' => 'expenses#new'
-	post 'group/:group_id' => 'expenses#create'
+	get '/group/:group_id' => 'groups#show'
+	get '/expense/:expense_id' => 'expenses#show'
+	get '/expense/:group_id/new' => 'expenses#new'
+	post '/group/:group_id' => 'expenses#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
