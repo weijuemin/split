d413df0f916628c8224ac0d6d942ae91c3c8f391
Brad Wagner <bradwagner@outlook.com>
2016-08-23 18:03:26 +0000

    add username

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
 
 	EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
   has_attached_file :profilepic
-  validates  :first_name,:last_name, presence: true
+  validates  :first_name, :username, :last_name, presence: true
   validates  :email, presence: true, uniqueness: {case_sensitive: false}, format: {with: EMAIL_REGEX}
   validates_attachment_content_type :profilepic, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
